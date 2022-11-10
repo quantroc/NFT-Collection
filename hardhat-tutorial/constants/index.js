1600c5055c1a98ea954ef844f94d1aef448be078
quantroc <ducquannguyen28699@gmail.com>
2022-11-10 16:11:10 +0700

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x1B23956ACcCf7D99188b367fD51047bEdFF4Bd0B";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
